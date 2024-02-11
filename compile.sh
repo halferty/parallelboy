2e2ddc250e8fad33944f2908d4031c03b98a6d47
Edward Halferty <ed@halferty.dev>
2024-02-10 23:18:00 -0800

    Add some dev notes about MBCs

--- compile.sh
+++ compile.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+./clean.sh
 ruby ./tools/merge_asm_files.rb
 rgbasm -L -o ./tmp/compiled.o ./tmp/compiled.asm
 rgblink -o ./build/parallelboy.gb ./tmp/compiled.o
-rgbfix -v -p 0xFF ./build/parallelboy.gb
+# v: validate (fix all the header values - meaning logo & checksums)
+# c: Gameboy color compatible
+# s: Super Gameboy compatible
+# p: Pad value (why 0xFF? Do some emulators break if it's something else?)
+# l: Licensee code (0x33 is the standard code for unlicensed games)
+# m: Memory bank controller type
+# r: RAM size (0x3 = 32KB, 4 banks of 8KB each)
+# t: Title
+# (I wanted to use -C, which is supposed to set the cartridge as Gameboy Color ONLY, but it isn't working with various emulators?!?)
+rgbfix -vcs -p 0xFF -l 0x33 -m MBC5+RAM+BATTERY -r 0x3 -t "Parallel Boy" ./build/parallelboy.gb
--- src/01_entrypoint.asm
+++ src/01_entrypoint.asm
@@ -2,3 +2,13 @@ EntryPoint:
 	; Shut down audio circuitry
 	ld a, 0
 	ld [rNR52], a
+
+; Banks
+; 0000 - 3FFF: ROM bank 0, first 16KiB of ROM (read only)
+; 4000 - 7FFF: ROM bank 0-1FF can be mapped here (read only)
+; A000 - BFFF: RAM bank 0-F can be mapped here (read/write) (Note that when calling rgbfix in compile.sh we choose only 4 banks, so only 0-3 are valid)
+
+; To switch banks:
+; Write lower 8 bits of bank number to 0x2000 and upper 1 bit to 0x3000 to select a ROM bank
+; Write to 0x4000 to select a RAM bank
+; Write 0x0A to 0x0000 to enable read/write to RAM. Write 0x00 to dasable it. (Why would we ever want to disable it? Does this reduce RAM corruption risk?)
